21cf513bfc6d222e997265486536f9e422fe20a3
carla <cfpcarla@gmail.com>
2020-06-30 15:20:03 -0400

    Exercise02

--- src/exercises.cljs
+++ src/exercises.cljs
@@ -10,3 +10,11 @@ defn process-value
         (> value 10) :pretty-big
         (< value 0) :negative
         :else :small-number))
+
+
+; Exercise02
+; Write a function to determine if some 3 side lengths are sufficient to make a triangle.
+
+; To check if 3 sides make a triangle, you need to check that every side is less than or equal to the sum of the other two sides.
+
+(defn triangle? [a b c] (and (< a (+ b c)) (< b (+ a c)) (< c (+
