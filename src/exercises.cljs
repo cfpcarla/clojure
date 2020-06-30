519a3d7e632e893327f98c0c96a57413e4ab878f
carla <cfpcarla@gmail.com>
2020-06-30 15:24:20 -0400

    Exercise05 and exercise06

--- src/exercises.cljs
+++ src/exercises.cljs
@@ -42,3 +42,47 @@ defn process-value
         (< x min) min
         :else max))
 
+;; Exercise 05
+;; Write a function largest to find the largest number in a list, making use of max and either apply or reduce.
+
+(defn largest [nums] (apply max nums))
+
+(defn largest-reduce [nums] (reduce max nums))
+
+; Exercise06
+; Implement a function score that calculates the scrabble score for a given word.
+; Use google to find out how much each letter is worth.
+
+(def points
+  {\a 1
+   \b 3
+   \c 3
+   \d 2
+   \e 1
+   \f 4
+   \g 2
+   \h 4
+   \i 1
+   \j 8
+   \k 5
+   \l 1
+   \m 3
+   \n 1
+   \o 1
+   \p 3
+   \q 10
+   \r 1
+   \s 1
+   \t 1
+   \u 1
+   \v 4
+   \w 4
+   \x 8
+   \y 4
+   \z 10})
+
+(defn score
+  [s]
+  (->> s
+       (map points)
+       (apply +)))
