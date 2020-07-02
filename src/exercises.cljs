24b80943f0e8f7067009d1dcbd7a69a504130b7a
carla <cfpcarla@gmail.com>
2020-07-02 11:41:59 -0400

    Exercise10

--- src/exercises.cljs
+++ src/exercises.cljs
@@ -134,3 +134,11 @@ defn process-value
 (defn moves-played [state] (count (state :history)))
 
 (defn moves-played-alt [state] (count (remove nil? (state :board))))
+
+;; Exercise 10
+;; Write a function get-and-set that makes a change to a collection at some location, and return the original value at that location and the changed collection.
+;; get-and-set should take 3 arguments: a key, a value, and a collection (a vector or map).
+;; It should return a vector of two things: (1) the value that was at that key in the original collection, and (2) the updated collection.
+
+
+(defn get-and-set [k v col] [(get col k) (assoc col k v)])
