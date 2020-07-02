34595af9df2f479e7700052e9b34f00a13551090
carla <cfpcarla@gmail.com>
2020-07-02 11:43:27 -0400

    Exercise12

--- src/exercises.cljs
+++ src/exercises.cljs
@@ -153,3 +153,14 @@ defn process-value
   ->> (slurp file-name)
   string/reverse
   (spit (str "rev-" file-name))))
+
+;; Exercise 12
+;; Write a function that takes a map with this structure:
+
+;; {:people {1 {:name "james"
+;;              :points 1}
+;;           2 {:name "rafd"
+;;              :points 5}}}
+;; as well as an id, a keyword, and some value. Return the same map but with the person with the given id having the given keyword & value added to their data (see tests for an example).
+
+(defn add-info [info id key value] (assoc-in info [:people id key] value))
