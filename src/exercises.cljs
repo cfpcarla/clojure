87d6f6d630918b7e8a7c60fe80cc7f0ee573ae63
carla <cfpcarla@gmail.com>
2020-07-02 11:45:42 -0400

    Exercise16

--- src/exercises.cljs
+++ src/exercises.cljs
@@ -198,3 +198,14 @@ defn process-value
       (dissoc :disabled?)
       (assoc :activated "now")
       (update :points inc)))
+
+;; exercise 16  - partial-map
+;; Write a function that takes two functions and a list. Transform the given list by applying the first function to all the values in the list that return true for the second function; other values should remain as is.
+
+defn partial-map
+  [f pred? values]
+  (map (fn [x]
+         (if (pred? x)
+          (f x)
+          x))
+       values))
